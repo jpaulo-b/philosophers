8a2f18b3a2528ad7f51a9c6dc679c7f372316244
Jorge Paulo Bravo Abreu Santos Pereira <jpaulo-b@c1r12s9.42porto.com>
2026-05-25 12:24:37 +0100

    last fixes

---
+++ philo/threads.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   threads.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpaulo-b <jpaulo-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/19 15:56:34 by jpaulo-b          #+#    #+#             */
+/*   Updated: 2026/05/21 15:41:11 by jpaulo-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+/*  Routine for each philosopher thread */
+void	*philosopher_routine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	if (philo->num_of_philos == 1)
+	{
+		pthread_mutex_lock(philo->l_fork);
+		print_action(philo, "has taken a fork");
+		smart_sleep(philo->time_to_die, philo);
+		pthread_mutex_unlock(philo->l_fork);
+		return (NULL);
+	}
+	if (philo->id % 2 == 0)
+		usleep(1000);
+	while (!is_dead(philo))
+	{
+		take_forks(philo);
+		eat_and_release(philo);
+		if (all_meals_done(philo))
+			break ;
+		print_action(philo, "is sleeping");
+		smart_sleep(philo->time_to_sleep, philo);
+		print_action(philo, "is thinking");
+	}
+	return (NULL);
+}
+
+/*  Check if all philosophers are satisfied */
+static int	philo_satisfied(t_info *info)
+{
+	int	full_count;
+
+	full_count = count_satisfied(info);
+	if (info->philo[0].num_times_to_eat > 0
+		&& full_count == info->philo[0].num_of_philos)
+		return (1);
+	return (0);
+}
+
+/*  Monitor routine to check for philosopher deaths and satisfaction */
+void	*monitor_routine(void *arg)
+{
+	t_info	*info;
+
+	info = (t_info *)arg;
+	while (1)
+	{
+		if (check_death(info))
+			return (NULL);
+		if (philo_satisfied(info))
+		{
+			set_dead(&info->philo[0]);
+			return (NULL);
+		}
+		if (is_dead(&info->philo[0]))
+			return (NULL);
+		usleep(1000);
+	}
+	return (NULL);
+}
---
+++ philo/utils.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpaulo-b <jpaulo-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/19 15:56:41 by jpaulo-b          #+#    #+#             */
+/*   Updated: 2026/05/25 11:38:25 by jpaulo-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+/*  Convert a string to an integer to use in init.c  */
+int	ft_atoi(const char *str)
+{
+	long long	number;
+
+	number = 0;
+	if (*str == '\0')
+		return (-1);
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return (-1);
+		number = number * 10 + (*str - '0');
+		if (number > 2147483647)
+			return (-1);
+		str++;
+	}
+	if (number <= 0)
+		return (-1);
+	return ((int)number);
+}
+
+/*  Get the current time in milliseconds */
+size_t	get_time(void)
+{
+	struct timeval	current;
+
+	if (gettimeofday(&current, NULL) != 0)
+		return (0);
+	return ((size_t)current.tv_sec * 1000ull
+		+ (size_t)current.tv_usec / 1000ull);
+}
+
+/*  Sleep for a specified time, checking for death */
+void	smart_sleep(size_t time, t_philo *philo)
+{
+	size_t	start;
+
+	start = get_time();
+	while (!is_dead(philo))
+	{
+		if (get_time() - start >= time)
+			break ;
+		usleep(250);
+	}
+}
+
+/*  Check if all required meals are done */
+int	all_meals_done(t_philo *philo)
+{
+	if (philo->num_times_to_eat > 0
+		&& philo->meals_eaten >= philo->num_times_to_eat)
+		return (1);
+	return (0);
+}
